66a9cf7167dac78569a146c68b193f3a010c928e
harshita sethiya <harshita.sethiya252@gmail.com>
2023-07-26 13:53:41 +0530

    demo

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+    <div>
+        <h2>FOOD RS 10</h2>
+    <div>
+        <h2>Petrol RS 100</h2>
+        <h2>Movies RS 200</h2>
+    </div>
+     </div>
+     );
+}
+export default ExpenseItem;
